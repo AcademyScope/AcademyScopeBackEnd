bd2e7eb5a6c21b8b95cdaed7d86d79ba823a9ff8
Volkan Orhan <contactme@volkanorhan.com>
2025-10-24 03:06:34 +0300

    Implements data model for program data
    Introduces a data model using QAbstractTableModel to manage and display program data.
    
    This model supports lazy loading, filtering, and sorting of program information retrieved from a SQLite database. It also includes functionality to show/hide columns, improving data presentation.
    
    The changes replace the previous direct table manipulation with a more maintainable and scalable approach.

--- Src/BackEnd.hpp
+++ Src/BackEnd.hpp
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License along with thi
 #include "EnumDefinitions.hpp"
 #include <QStandardItemModel>
 #include <optional>
+#include "AcademyScopeModel.hpp"
 
 struct University {
     int id;
@@ -131,10 +132,11 @@ protected:
 
 class AcademyScopeBackEnd {
 public:
-    AcademyScopeBackEnd(ProgramTableInterface * programTable);
+    AcademyScopeBackEnd();
     QList<University> getUniversities();
     QList<QString> getDepartments();
     void populateProgramTable(const AcademyScopeParameters &academyScopeParameters);
+    AcademyScopeModel * getDataModel();
 
 private:
     void initDB();
@@ -145,9 +147,9 @@ private:
     void hideUnusedColumnsOnTheProgramTable();
     void initializeYKSTableColumnNames();
     void setLogoDarkMode(bool isDarkMode);
+    QString buildFilteredSql(const AcademyScopeParameters &academyScopeParameters);
     QString getDbColumnNameFromProgramTableColumnIndex(ProgramTableColumn columnIndex);
-
-    ProgramTableInterface * programTable = nullptr;
+    AcademyScopeModel dataModel;
 
     QLocale turkishLocale;
     QStringList yksTableColumnNames;
